faa15b5933c35e84c2fa001e53628a509788a062
nerte <grimza@nerteui-MacBookPro.local>
2025-01-06 01:46:13 +0900

    [Feat] create Error State Error의 유무를 값으로 갖는 state 생성, 데이터를 더 받아올때 에러가 발생시 해당 스테이트 값을 에러객체로 변경해서 Error가 발생시 화면에 메시지를 띄울수있게 함.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,13 @@
-export default async function getFoods(order = "") {
-  const query = `?order=${order}`;
+export default async function getFoods({
+  order = "createdAt",
+  limit = 10,
+  cursor = "",
+}) {
+  const query = `?order=${order}&cursor=${cursor}&limit=${limit}`;
   const response = await fetch(`https://learn.codeit.kr/0909/foods/${query}`);
+  if (!response.ok) {
+    throw new Error("데이터를 불러오는데 실패했습니다.");
+  }
   const body = await response.json();
   return body;
 }
--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import getFoods from "../api";
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState("");
+  const [isError, setIsErorr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const orderItems = items.sort((a, b) => b[order] - a[order]);
   const handleCalorieClick = () => {
     setOrder("calorie");
@@ -12,18 +15,44 @@ function App() {
   const handleNewsteCLick = () => {
     setOrder("createdAt");
   };
+
   const handelDelete = (id) => {
     const nextItmes = items.filter((item) => item.id !== id);
     setItems(nextItmes);
   };
-  const handleLoad = async (orderQuery) => {
-    const { foods } = await getFoods(orderQuery);
-    setItems(foods);
+
+  const handleLoad = async (options) => {
+    let result;
+    try {
+      setIsLoading(true);
+      setIsErorr(null);
+      result = await getFoods(options);
+    } catch (error) {
+      setIsErorr(error);
+    } finally {
+      setIsLoading(false);
+    }
+    const {
+      foods,
+      paging: { nextCursor },
+    } = result;
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
   };
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order });
   }, [order]);
 
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
+  };
   return (
     <div>
       <div>
@@ -31,6 +60,13 @@ function App() {
         <button onClick={handleCalorieClick}>칼로리순</button>
       </div>
       <FoodList items={orderItems} onDelete={handelDelete} />
+
+      {cursor && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더보기
+        </button>
+      )}
+      {isError?.message && <p>{isError.message}</p>}
     </div>
   );
 }
